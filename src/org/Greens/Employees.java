1cbf27bea940cb156bc3e99ef94f1948f50b6469
rajigitacc <rajathi.ramesh@outlook.com>
2021-11-23 04:12:09 +0400

    test

---
+++ src/org/Greens/Employees.java
@@ -0,0 +1,32 @@
+package org.Greens;
+
+import java.util.Scanner;
+
+public class Employees { 	
+
+	
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the Employee name:");
+
+    String empName=sc.nextLine();
+    System.out.println("the emp name:" +empName );	
+    
+    System.out.println("Enter the Employee email:");
+
+    String empEmail=sc.nextLine();
+    System.out.println("the emp email:" +empEmail);
+	
+	System.out.println("Enter the Employee Id:");
+     
+    byte empid=sc.nextByte();
+    System.out.println("the emp id is:" +empid);
+    
+    System.out.println("Enter the Phone Id:");
+    
+    long phoneNo=sc.nextLong();
+    System.out.println("the emp phone number is:" +phoneNo);
+  
+}
+
+}
